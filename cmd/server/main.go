c21616840bd11837b8ebaec00345c040e0bfa90c
jacobh18 <jacobhuneke@gmail.com>
2026-03-19 20:06:53 -0700

    A: server main

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Starting Peril server...")
+	connString := "amqp://guest:guest@localhost:5672/"
+	connection, err := amqp.Dial(connString)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	defer connection.Close()
+	fmt.Println("Successfully connected")
+
+	channel, err := connection.Channel()
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	exchange := routing.ExchangePerilDirect
+	key := routing.PauseKey
+	val := routing.PlayingState{
+		IsPaused: true,
+	}
+	err = pubsub.PublishJSON(channel, exchange, key, val)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
+	fmt.Println("Server is shutting down")
 }
